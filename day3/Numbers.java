1540aefaaa8416fa38c9e985e1e21a4db2041624
anujachaitanya <anujachaitanya09@gmail.com>
2020-10-24 09:25:33 +0530

    added average, isAscending and range in Numbers

--- day3/Numbers.java
+++ day3/Numbers.java
@@ -24,6 +24,40 @@ public class Numbers {
     }
   }
 
+  public static double average(int[] numbers) {
+    int sum = 0;
+
+    for (int index = 0; index < numbers.length; index++) {
+      sum += numbers[index];
+    }
+    return (sum / (double) numbers.length);
+  }
+
+  public static boolean isAscending(int[] numbers) {
+    for (int index = 0; index < numbers.length - 1; index++) {
+      if (numbers[index] >= numbers[index + 1]) {
+        return false;
+      }
+    }
+    return true;
+  }
+
+  public static int[] range(int start, int end, int step) {
+    int totalNumbers = (end - start) / step + 1;
+    int[] rangeOfNumbers = new int[totalNumbers];
+    int currentNumber = start;
+
+    for (int index = 0; index < totalNumbers; index++) {
+      rangeOfNumbers[index] = currentNumber;
+      currentNumber += step;
+    }
+    return rangeOfNumbers;
+  }
+
+  public static int[] range(int start, int end) {
+    return range(start, end, 1);
+  }
+
   public static void main(String[] args) {
     int[] array = repeat(3, 4);
     int[] a = { 1, 2, 3, 4 };
