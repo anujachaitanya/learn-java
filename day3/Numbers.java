26fa248c097d570b1b98f7a8d430f148d19e61e6
anujachaitanya <anujachaitanya09@gmail.com>
2020-10-24 09:53:19 +0530

    addd concatination

--- day3/Numbers.java
+++ day3/Numbers.java
@@ -2,17 +2,17 @@ public class Numbers {
 
   public static int[] repeat(int number, int n) {
     int[] repeated = new int[n];
-    for (int i = 0; i < n; i++) {
-      repeated[i] = number;
+    for (int index = 0; index < n; index++) {
+      repeated[index] = number;
     }
     return repeated;
   }
 
   public static int findLargest(int[] array) {
     int largest = array[0];
-    for (int i = 0; i < array.length; i++) {
-      if (largest < array[i]) {
-        largest = array[i];
+    for (int index = 0; index < array.length; index++) {
+      if (largest < array[index]) {
+        largest = array[index];
       }
     }
     return largest;
@@ -40,6 +40,20 @@ public class Numbers {
       }
     }
     return true;
+  }
+
+  public static int[] concatenate(int[] array1, int[] array2) {
+    int[] newArray = new int[array1.length + array2.length];
+
+    for (int index = 0; index < newArray.length; index++) {
+      if(index < array1.length) {
+        newArray[index] = array1[index]
+      } else {
+        newArray[index] = array2[index - array1.length];
+      }
+    }
+
+    return newArray;
   }
 
   public static int[] range(int start, int end, int step) {
